c52e5ad79dac911ef363c32d33949c148021e2ca
Shashank Keshava <shashank_k@intuit.com>
2022-08-21 21:58:01 +0530

    using spring config file

---
+++ src/com/luv2code/springdemo/HelloSpringApp.java
@@ -0,0 +1,23 @@
+package com.luv2code.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class HelloSpringApp {
+
+	public static void main(String[] args) {
+	
+	// Load the spring configuration file
+	ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+	// retrieve bean from spring container
+	Coach theCoach = context.getBean("myCoach",Coach.class);
+	
+	// call methods on the bean
+	System.out.println(theCoach.getDailyWorkout());
+		
+	// Close the context
+	context.close();
+
+	}
+
+}
