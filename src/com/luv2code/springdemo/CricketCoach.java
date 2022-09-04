9d0376f2790a0613302ec16f2c29da43eaddf046
Shashank Keshava <shashank_k@intuit.com>
2022-09-04 18:30:44 +0530

    Assignment 1

---
+++ src/com/luv2code/springdemo/CricketCoach.java
@@ -0,0 +1,12 @@
+package com.luv2code.springdemo;
+
+public class CricketCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		// TODO Auto-generated method stub
+		return "Practice 1 hours batting";
+	}
+	
+
+}
