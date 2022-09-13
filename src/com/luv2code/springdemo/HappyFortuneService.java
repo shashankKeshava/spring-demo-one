eda2e15f64bdedbcf624618888ff3bd939f447d9
Shashank Keshava <skeshava@Shashanks-MacBook-Pro.local>
2022-09-13 21:19:06 +0530

    section 5 assignment

--- src/com/luv2code/springdemo/HappyFortuneService.java
+++ src/com/luv2code/springdemo/HappyFortuneService.java
@@ -1,10 +1,14 @@
 package com.luv2code.springdemo;
 
 public class HappyFortuneService implements FortuneService {
+	
+	private String[] Fortunes= {"won","loss","draw"};
+	
+	private int Random=(int)(Math.random()*(3-0))+0;
 
 	@Override
 	public String getFortune() {
-		return "Today is my lucky day";
+		return Fortunes[Random];
 	}
 
 }
